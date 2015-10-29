fd885aa728d1b3645d073bfcaebd10ee23e2872e
Ashish Rana <deviced.in@gmail.com>
2015-10-30 04:43:35 +0530

    so far and some more images to explain concept better

--- client/home/home.js
+++ client/home/home.js
@@ -26,4 +26,44 @@ Template.home.rendered = function() {
         player.api('pause');
       });
   });
+
+  $(function(){
+    $(document).scroll(function(){
+      //console.log($(this).scrollTop());
+
+      // TODO: make it a proper function with can input 1). element, 2). css property, 3). value, 4). value condition function
+      // for the range the css property should have a set value.
+      // get the current value.
+      // match with the expected value
+      // if no match, change
+      /*
+      {
+        ele : $('#qbila-forGovernment .d-bgImage'),
+        cssProp : 'background-attachment',
+        conditionalValue : {
+
+        }
+      }
+      */
+      var govBg = $('#qbila-forGovernment .d-bgImage').css('background-attachment')
+//console.log('asdf');
+      if ( $(this).scrollTop() > $('#qbila-forGovernment').offset().top && govBg != 'fixed' ) {
+        // fixed
+        $('#qbila-forGovernment .d-bgImage2').css('background-image', 'none');
+        //$('#qbila-forGovernment .d-bgImage').css('background-attachment', 'fixed');
+        //$('#qbila-forGovernment .d-bgImage').css({ '-webkit-transform' : 'translate3d(0,0,0);' });
+        //console.log('switched to fixed');
+      }
+      //console.log('zxvc');
+     if ( $(this).scrollTop() <= $('#qbila-forGovernment').offset().top  && govBg != 'scroll' ) {
+        // scroll
+        //$('#qbila-forGovernment .d-bgImage').css({'background-attachment' : 'scroll'});
+        //console.log('switched to scroll');
+      }
+
+      // $('#qbila-forGovernment .d-bgImage').css({'background-attachment' : $(this).scrollTop() > $('#qbila-forGovernment').offset().top ? "fixed":"scroll"});
+      // console.log($('#qbila-forGovernment .d-bgImage').css('background-attachment'));
+      // change when is not as desired
+    });
+  });
 };
